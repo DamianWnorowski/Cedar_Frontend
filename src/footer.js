c6fecbc355c14748bf6a08830d12b56ca27c8633
DamianWnorowski <damianwnoro@gmail.com>
2018-04-07 17:49:48 -0400

    footer file

---
+++ src/footer.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import './App.css';
+import { Grid, Image, Container,  List, Header,Divider,  Segment} from 'semantic-ui-react'
+
+
+
+const Footer = () =>{
+    return (
+        <Segment
+            inverted
+            vertical
+            style={{ margin: '5em 0em 0em', }}
+        >
+            <Container textAlign='center'>
+            <List horizontal inverted divided link>
+                <List.Item as='a' href='#'>Site Map</List.Item>
+                <List.Item as='a' href='#'>Contact Us</List.Item>
+                <List.Item as='a' href='#'>Terms and Conditions</List.Item>
+                <List.Item as='a' href='#'>Privacy Policy</List.Item>
+            </List>
+            </Container>
+        </Segment>
+    )
+}
+
+export default Footer
