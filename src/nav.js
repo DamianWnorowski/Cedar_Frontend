e3eec5662a1fdd7def2b489672b37fe1ba36e4a5
Damian Wnorowski <DamianWnoro@gmail.com>
2018-04-22 04:19:31 -0400

    search page

--- src/nav.js
+++ src/nav.js
@@ -51,10 +51,9 @@ class Nav extends Component {
                 <Grid.Column width={3}>
                     <Header textAlign='right' size='large' style={{color:'white'}}>Cedar</Header>
                 </Grid.Column>
-                <Grid.Column width={10}>
+                <Grid.Column width={8}>
                     <Input
                     fluid
-                    icon={<Icon name='search' circular link onClick={this.onSearch}/> }
                     placeholder='Search...'
                     onChange={this.onSearchText}
                     onKeyPress={this.onSearchEnterPress}
@@ -63,6 +62,9 @@ class Nav extends Component {
                         <Menu.Item as={ Link } to='/' color={'blue'} style={{color:'white'}} name={home} active={activeItem === home} onClick={this.handleItemClick} />
                         <Menu.Item as={ Link } to='/browse' color={'blue'} style={{color:'white'}} name={browse} active={activeItem ===  browse} onClick={this.handleItemClick} />
                     </Menu>
+                </Grid.Column>
+                <Grid.Column width={2} style={{paddingLeft:0,paddingTop:5, marginLeft:'-3.5em'}}>
+                    <Link to={'/search/' + this.state.search}  ><Icon inverted name='search' color='blue' circular link onClick={this.onSearch}/></Link>
                 </Grid.Column>
                 <Grid.Column width={3}>
                     <Menu inverted stackable style={{backgroundColor:'black', margin:0}}>
