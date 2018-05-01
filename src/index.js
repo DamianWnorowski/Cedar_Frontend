4fdbc8a9ac5a8c9be2403245ff5cc6a3f087a3f2
Serge Crane <serge.crane@stonybrook.edu>
2018-05-01 18:26:26 -0400

    login persists page reload

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import setAuthToken from './setAuthToken'
+
+setAuthToken(localStorage.getItem("jwtToken"));
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
--- src/nav.js
+++ src/nav.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 import { Grid,  Input, Container, Menu, Header,Popup,  Button , Form, Segment, Icon} from 'semantic-ui-react';
 import axios from 'axios';
+import setAuthToken from './setAuthToken'
 
 const home = 'home';
 const browse = 'browse';
@@ -67,14 +68,16 @@ class Nav extends Component {
                     console.log(error);
         });               
     }
-    
+       
     handleLoginSubmit = (e) => {
         e.preventDefault();   
         const {email, password} = this.state;
         axios.post(`http://localhost:8080/login`, {email, password})
                 .then((result) => {
                     this.setState({login:true})
-                    axios.defaults.headers.common['Authorization'] = "Bearer " + result.data
+                    const token = result.data
+                    localStorage.setItem('jwtToken', token)
+                    setAuthToken(token)
                     console.log(result.data);
         })
                 .catch((error) => {
