d6b3939313597cc3c2bba169fad712aaad7e10e1
Justin <Justin.Gartenberg@stonybrook.edu>
2018-05-09 02:15:56 -0400

    new tv tonight

--- src/homePage.js
+++ src/homePage.js
@@ -19,67 +19,67 @@ const MOVIES = constants.MOVIES;
 
 const imgUrl = constants.IMGURL;
 const movieInfo = {written:'Frankie Fry', runtime:'120min', boxoffice:'$207 million', studio:'Disney', title: MOVIES[6].title, poster_path: MOVIES[6].poster_path, genre: 'Animated', released: MOVIES[6].release_date, directed: 'John Smith', overview: MOVIES[6].overview }
-const tableItem1= {rating: '60%', title: 'cinderella', sales: '100M'}
-const tableItem2= {rating: '90%', title: 'fire', sales: '150M'}
-const tableItem3= {rating: '30%', title: 'twitch', sales: '20M'}
-const tableItem4= {rating: '50%', title: 'all about eve', sales: '20M'}
-const tableItem5= {rating: '10%', title: 'frankie', sales: '120M'}
-const tableItem6= {rating: '35%', title: 'Fishng', sales: '250M'}
-const tableItem7= {rating: '36%', title: 'batman', sales: '80M'}
-const tableItem8= {rating: '32%', title: 'lino', sales: '2M'}
-const tableItem9= {rating: '66%', title: 'batman2', sales: '60M'}
-const tableItem10= {rating: '72%', title: 'pRESIDENT', sales: '212M'}
-const tableList = [tableItem1, tableItem2, tableItem3, tableItem4, tableItem5, tableItem6, tableItem7, tableItem8, tableItem9, tableItem10]
+// const tableItem1= {rating: '60%', title: 'cinderella', sales: '100M'}
+// const tableItem2= {rating: '90%', title: 'fire', sales: '150M'}
+// const tableItem3= {rating: '30%', title: 'twitch', sales: '20M'}
+// const tableItem4= {rating: '50%', title: 'all about eve', sales: '20M'}
+// const tableItem5= {rating: '10%', title: 'frankie', sales: '120M'}
+// const tableItem6= {rating: '35%', title: 'Fishng', sales: '250M'}
+// const tableItem7= {rating: '36%', title: 'batman', sales: '80M'}
+// const tableItem8= {rating: '32%', title: 'lino', sales: '2M'}
+// const tableItem9= {rating: '66%', title: 'batman2', sales: '60M'}
+// const tableItem10= {rating: '72%', title: 'pRESIDENT', sales: '212M'}
+// const tableList = [tableItem1, tableItem2, tableItem3, tableItem4, tableItem5, tableItem6, tableItem7, tableItem8, tableItem9, tableItem10]
 let altColor = false
 
-const tableMap = tableList.map(item =>{
-    if(altColor){
-        altColor = false
-    }else{
-        altColor = true
-    }
-    return (<Grid.Row 
-    key = {item.title+'9'}
-    columns='equal'  
-    className={(altColor) ? 'style2' : 'style1'}
-    style={{padding:0,paddingTop:'.1em',paddingBottom:'.1em', borderLeft: '.3em solid rgba(2, 199, 255, 0.1)'}}
-    >
-        <Grid.Column style={{padding:0, paddingLeft:'.5em'}} textAlign='left'>
-        {item.rating}
-        </Grid.Column>
-        <Grid.Column width={10} style={{padding: 0,color:'#02c7ff'}}>
-        {item.title}
-        </Grid.Column>
-        <Grid.Column style={{padding:0, paddingRight:'.5em'}} >
-        {item.sales}
-        </Grid.Column>
-    </Grid.Row>)
-});
-const tableMapTv = tableList.map(item =>{
-    if(altColor){
-        altColor = false
-    }else{
-        altColor = true
-    }
-    return (
-        <Grid.Row 
-    key = {item.title+'9'}
-    columns='equal'  
-    className={(altColor) ? 'style2' : 'style1'}
-    style={{padding:0,paddingTop:'.1em',paddingBottom:'.1em', borderLeft: '.3em solid rgba(2, 199, 255, 0.1)'}}
-    >
-        <Grid.Column style={{padding:0, paddingLeft:'.5em'}} textAlign='left'>
-        {item.rating}
-        </Grid.Column>
-        <Grid.Column width={10} style={{padding: 0,color:'#02c7ff'}}>
-        {item.title}
-        </Grid.Column>
-        <Grid.Column style={{padding:0, paddingRight:'.5em'}} >
-        {item.sales}
-        </Grid.Column>
-    </Grid.Row>
-    )
-});
+// const tableMap = tableList.map(item =>{
+//     if(altColor){
+//         altColor = false
+//     }else{
+//         altColor = true
+//     }
+//     return (<Grid.Row 
+//     key = {item.title+'9'}
+//     columns='equal'  
+//     className={(altColor) ? 'style2' : 'style1'}
+//     style={{padding:0,paddingTop:'.1em',paddingBottom:'.1em', borderLeft: '.3em solid rgba(2, 199, 255, 0.1)'}}
+//     >
+//         <Grid.Column style={{padding:0, paddingLeft:'.5em'}} textAlign='left'>
+//         {item.rating}
+//         </Grid.Column>
+//         <Grid.Column width={10} style={{padding: 0,color:'#02c7ff'}}>
+//         {item.title}
+//         </Grid.Column>
+//         <Grid.Column style={{padding:0, paddingRight:'.5em'}} >
+//         {item.sales}
+//         </Grid.Column>
+//     </Grid.Row>)
+// });
+// const tableMapTv = tableList.map(item =>{
+//     if(altColor){
+//         altColor = false
+//     }else{
+//         altColor = true
+//     }
+//     return (
+//         <Grid.Row 
+//     key = {item.title+'9'}
+//     columns='equal'  
+//     className={(altColor) ? 'style2' : 'style1'}
+//     style={{padding:0,paddingTop:'.1em',paddingBottom:'.1em', borderLeft: '.3em solid rgba(2, 199, 255, 0.1)'}}
+//     >
+//         <Grid.Column style={{padding:0, paddingLeft:'.5em'}} textAlign='left'>
+//         {item.rating}
+//         </Grid.Column>
+//         <Grid.Column width={10} style={{padding: 0,color:'#02c7ff'}}>
+//         {item.title}
+//         </Grid.Column>
+//         <Grid.Column style={{padding:0, paddingRight:'.5em'}} >
+//         {item.sales}
+//         </Grid.Column>
+//     </Grid.Row>
+//     )
+// });
 
 const boxOfficeTable = (data) => {
     let altColor = false;
@@ -106,6 +106,30 @@ const boxOfficeTable = (data) => {
     })
     return table;
 }
+
+const tvTable = (data) => {
+    let altColor = false;
+    const table = data.map(item => {
+        altColor = !altColor
+        return (
+            <Grid.Row 
+            key = {item.title+'9'}
+            columns='equal'  
+            className={(altColor) ? 'style2' : 'style1'}
+            style={{padding:0,paddingTop:'.1em',paddingBottom:'.1em', borderLeft: '.3em solid rgba(2, 199, 255, 0.1)'}}
+            >
+                <Grid.Column style={{padding:0, paddingLeft:'.5em'}} textAlign='left'>
+                {item.criticRating + '%'}
+                </Grid.Column>
+                <Grid.Column width={10} style={{padding: 0,color:'#02c7ff'}}>
+                {item.title}
+                </Grid.Column>
+            </Grid.Row>
+        )
+    })
+    return table;
+}
+
 
 const officeTable = (data) => {
     let altColor = false;
@@ -156,6 +180,7 @@ class HomePage extends Component {
             renderMoviesComingSoon: false,
             renderMoviesUpcoming: false,
             renderTopBoxOffice: false,
+            renderNewTVTonight: false,
             verified: false,
         }
     }
@@ -190,6 +215,13 @@ class HomePage extends Component {
                 if(res.data.length){
                     const topBoxOffice = boxOfficeTable(res.data);
                     this.setState({topBoxOffice, renderTopBoxOffice: true})
+                }
+        })
+        axios.get(`http://localhost:8080/api/newtvtonight`)
+            .then(res => {
+                if(res.data.length){
+                    const newTV = tvTable(res.data);
+                    this.setState({newTV, renderNewTVTonight: true})
                 }
         })
     }
@@ -235,7 +267,7 @@ class HomePage extends Component {
                         </Grid.Column>
                         <Grid.Column width={6}>
                             {(this.state.renderTopBoxOffice)? <MediaTable gridSize={12} displayInfo={this.state.topBoxOffice} numShow={6} nameHeader={<div>Top box<span style={{color:'white'}}>office</span></div>}/> : <div />}
-                            <MediaTable gridSize={12} displayInfo={officeTable(tableList)} numShow={6} nameHeader={<div>New tv<span style={{color:'white'}}>tonight</span></div>}/>
+                            {(this.state.renderNewTVTonight)?<MediaTable gridSize={12} displayInfo={this.state.newTV} numShow={6} nameHeader={<div>New tv<span style={{color:'white'}}>tonight</span></div>}/> : <div />}
                         </Grid.Column>
                     </Grid>
                 </Container>
